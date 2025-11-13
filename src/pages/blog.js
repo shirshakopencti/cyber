a06f60805b416fffd92fef9e6401b1471c688ec8
Shirshak Roy <shirshak831@gmail.com>
2025-11-13 23:52:58 +0530

    color hover

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,7 +11,6 @@ const PageWrapper = styled.div`
   max-width: 1000px;
   margin: 0 auto;
   padding: 4rem 2rem;
-  padding-top: calc(var(--nav-height) + 2rem);
 `;
 
 /* ----------  GRID  ---------- */
