9d31e6557ffe391de108220953c370449a29b514
Shirshak Roy <shirshak831@gmail.com>
2025-11-13 21:01:15 +0530

    reduced blog spacer to 10%

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -132,7 +132,7 @@ const Excerpt = styled.p`
 
 const Spacer = styled.div`
   height: var(--nav-height); /* 100px normally */
-  width: 10%;
+  width: 10000%;
 `;
 
 /* ----------  COMPONENT  ---------- */
